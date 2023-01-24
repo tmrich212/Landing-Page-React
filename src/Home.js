e7a378113573d1901b8d8ce270398ec7f6a84242
tmrich212 <57809945+tmrich212@users.noreply.github.com>
2023-01-23 20:19:55 -0500

    Init commit

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    const welcome = 'Welcome to the Future of Tech';
+
+
+    return ( 
+        <div className="greeting">
+            <h1>{ welcome }</h1>
+        </div>
+     );
+}
+ 
+export default Home;
